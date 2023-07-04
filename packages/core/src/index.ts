9ff1997f3fdc80aa9c4b8b0747b95b489b7f62a4
Adam Smith <ajs11174@gmail.com>
2023-07-04 21:44:20 +0000

    Fixed imports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
 export * from './abstract-provider/index.js';
 export * from './errors/index.js';
+export * from './jwt/index.js';
+export * from './providers/index.js';
 export * from './schema/index.js';
 export * from './util/index.js';
